f6133088b5252f78af2a79553acc352b03ae0f95
Philip Pavo <catl33t@gmail.com>
2015-01-23 09:41:23 +0000

    config template

---
+++ Config.php
@@ -0,0 +1,8 @@
+<?php
+	define("DB_PASSWORD", '');
+	define("DB_USERNAME", "");
+	define("DB_TABLE", "");
+	define("VK_API_VERSION", "");
+	define("VK_API_ID", "");
+	define("VK_API_SECRET", "");
+?>
